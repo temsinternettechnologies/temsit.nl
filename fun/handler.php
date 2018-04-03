1a1e80b67e5bf94b51392eaa648453aa2cda8d0e
spijkermenno <spijkermenno@gmail.com>
2018-04-03 13:02:16 +0200

    added subscription fields and made the /fun/ responsive.

---
+++ fun/handler.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116390520-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-116390520-1');
+    </script>
+</head>
+<body>
+<form method="post" action="/fun" id="form">
+    <input type="hidden" name="action" value="validate-subscription">
+    <a class="click">Je wordt omgeleid, geen zin om te wachten? klik hier!</a>
+</form>
+<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+<script>
+    $('.click').click(function () {
+        $('#form').submit();
+    });
+    $(document).ready(function () {
+        $("#form").submit();
+    })
+</script>
+</body>
+</html>
--- fun/index.php
+++ fun/index.php
@@ -17,31 +17,58 @@
 
     <!-- Custom styles for this template -->
     <link href="style.css" rel="stylesheet">
-    
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116390520-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-116390520-1');
-</script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116390520-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-116390520-1');
+    </script>
 
 </head>
 
 <body>
-<div class="col-lg-6 offset-lg-3 text-center bash">
-    <div class="bash-title">
-        <div class="buttons">
-            <div class="button bg-danger"></div>
-            <div class="button bg-warning"></div>
-            <div class="button bg-success"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-10 offset-lg-1 col-xs-12 text-center bash">
+            <div class="bash-title">
+                <div class="buttons">
+                    <div class="button bg-danger"></div>
+                    <div class="button bg-warning"></div>
+                    <div class="button bg-success"></div>
+                </div>
+                <p class="lead" style="display:inline-block; padding:0 100px; text-align: center; color: #000;">
+                    tems.exe</p>
+            </div>
+            <div class="bash-content text-left">
+                <h5 id="heading">$ </h5>
+            </div>
         </div>
-        <p class="lead" style="display:inline-block; width: 600px; padding:0 100px; text-align: center; color: #000;">tems.exe</p>
     </div>
-    <div class="bash-content text-left">
-        <h5 id="heading">$ </h5>
+    <br/>
+    <div class="row">
+        <div class="col-lg-10 offset-lg-1 col-xs-12 text-center">
+            <h5>Wilt u geinformeerd worden wanneer wij officieel van start gaan?<br/>Vul dit formulier in!</h5>
+            <br/>
+            <form method="post" action="handler.php">
+                <div class="col-lg-10 offset-lg-1 text-center" style="margin-bottom: 10px">
+                    <input class="form-control" type="text" name="name" placeholder="Voor en achternaam"/>
+                </div>
+                <div class="col-lg-10 offset-lg-1 text-center" style="margin-bottom: 10px">
+                    <input class="form-control" type="email" name="email" placeholder="E-mailadres"/>
+                </div>
+                <div class="col-lg-10 offset-lg-1 text-center" style="margin-bottom: 10px">
+                    <input class="form-control btn btn-warning" type="submit"/>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 
@@ -56,7 +83,7 @@
 <script src="https://mattboldt.com/demos/typed-js/js/typed.custom.js"></script>
 <script>
 
-    $(function(){
+    $(function () {
 
         $("#heading").typed({
             strings: ["Initinating 'TEMS Internet Technologies'..."],
