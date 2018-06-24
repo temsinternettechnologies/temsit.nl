994201d081546cc146ef99a7f37edd68b5ea7b2c
root <root@webmin.tychoengberink.nl>
2018-06-24 23:11:50 +0200

    init fix

--- init.php
+++ init.php
@@ -2,8 +2,8 @@
 require_once("classes/Database.class.php");
 
 define("HOST", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "root");
+define("USERNAME", "temsit");
+define("PASSWORD", "QuintIan055");
 define("DATABASE", "temsit");
 define("PORT", "3306");
 
