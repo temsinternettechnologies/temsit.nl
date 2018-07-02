9e6a2d3fb4a31c181285125f28345d4c11150d2a
Menno Spijker <spijkermenno@gmail.com>
2018-07-03 00:43:47 +0200

    quick fix #2

--- requires/functions.php
+++ requires/functions.php
@@ -1,15 +1,17 @@
 <?php
-function getCopyrightText(){
+function getCopyrightText()
+{
     $date = date("Y");
-    if ($date == "2018"){
+    if ($date == "2018") {
         return $date;
-    }else if (intval($date) > 2018){
+    } else if (intval($date) > 2018) {
         return "2018 - " . $date;
     }
     return false;
 }
 
-function getIP(){
+function getIP()
+{
     $ip = "unset";
 
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
@@ -22,15 +24,22 @@ function getIP(){
     return $ip;
 }
 
-function getRequestUri(){
-    if (strpos($_SERVER['REQUEST_URI'], "temsit.nl")){
-        $array = explode("/" , $_SERVER['REQUEST_URI']);
+function getRequestUri()
+{
+    if (strpos($_SERVER['REQUEST_URI'], "temsit.nl")) {
+        $array = explode("/", $_SERVER['REQUEST_URI']);
         $request = "/";
-        foreach ($array as $key => $value){
-            if ($value != "temsit.nl" && $value != ""){
+        foreach ($array as $key => $value) {
+            if ($value != "temsit.nl" && $value != "") {
                 $request .= $value . "/";
             }
         }
-            return $request;
+        return $request;
+    } else {
+        $array = explode("/", $_SERVER['REQUEST_URI']);
+        $request = "/";
+        foreach ($array as $key => $value) {
+            $request .= $value . "/";
+        }
     }
 }
