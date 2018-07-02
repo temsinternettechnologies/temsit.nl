f76e3adbed0c872d8132b0228d28d357cad0dc24
Menno Spijker <spijkermenno@gmail.com>
2018-07-03 01:03:15 +0200

    brand referrer

--- template/navbar.php
+++ template/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-light bg-light static-top">
     <div class="container">
-        <a class="navbar-brand" href="#">TEMS - IT</a>
+        <a class="navbar-brand" href="/">TEMS - IT</a>
         <a class="btn btn-info disabled" href="/glass/">Inloggen</a>
     </div>
 </nav>
