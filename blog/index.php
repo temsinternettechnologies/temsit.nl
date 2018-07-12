6d822d151d58911c62410a80bd3111808ffb5aa6
Menno Spijker <spijkermenno@gmail.com>
2018-07-13 01:00:25 +0200

    commit

--- blog/index.php
+++ blog/index.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ALL);
 require "../init.php";
 ?>
 <!DOCTYPE html>
@@ -22,7 +23,6 @@ if (!$value = Cookies::getCookie("TID")) {
 
 loadTemplate("navbar");
 $blogs = Database::select("SELECT * FROM blogs order by id desc limit 20");
-$blogs = null;
 if (count($blogs)) {
     echo "<div class='blogs pt-2 pb-2'><div class='container'><div class='row no-gutters'> ";
     $count = 0;
