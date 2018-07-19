6797e0320d3312388e1454dc51693dc4fb17d1de
Menno Spijker <spijkermenno@gmail.com>
2018-07-19 23:29:39 +0200

    ads styling.

--- blog/item/index.php
+++ blog/item/index.php
@@ -57,25 +57,27 @@ if (count($blog)) {
     <?php
 }
 ?>
-<script type="text/javascript">
-    (function () {
-        if (window.CHITIKA === undefined) {
-            window.CHITIKA = {'units': []};
-        }
-        ;
-        var unit = {
-            "calltype": "async[2]",
-            "publisher": "temsit",
-            "width": 728,
-            "height": 90,
-            "sid": "Chitika Default"
-        };
-        var placement_id = window.CHITIKA.units.length;
-        window.CHITIKA.units.push(unit);
-        document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
-    }());
-</script>
-<script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
+<div class="container">
+    <script type="text/javascript">
+        (function () {
+            if (window.CHITIKA === undefined) {
+                window.CHITIKA = {'units': []};
+            }
+            ;
+            var unit = {
+                "calltype": "async[2]",
+                "publisher": "temsit",
+                "width": 728,
+                "height": 90,
+                "sid": "Chitika Default"
+            };
+            var placement_id = window.CHITIKA.units.length;
+            window.CHITIKA.units.push(unit);
+            document.write('<div class="mx-auto" id="chitikaAdBlock-' + placement_id + '"></div>');
+        }());
+    </script>
+    <script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
+</div>
 <?php
 loadTemplate("footer");
 loadTemplate("scripts");
