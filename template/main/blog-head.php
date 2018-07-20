8840c4138bfd34ce674f8da39ff5d8e94d570c16
Menno Spijker <spijkermenno@gmail.com>
2018-07-20 11:06:13 +0200

    fixed build

--- template/main/blog-head.php
+++ template/main/blog-head.php
@@ -40,11 +40,11 @@ $blog = Database::select(sprintf("SELECT * FROM blogs where id = %d", $blogId))[
 <?php
 if ($_SERVER['REMOTE_ADDR'] != "::1") {
     ?>
-    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/style.css?v=<?=BUILD?>" rel="stylesheet">
     <?php
 }else {
     ?>
-    <link href="/temsit.nl/css/style.css" rel="stylesheet">
+    <link href="/temsit.nl/css/style.css?v=<?=BUILD?>" rel="stylesheet">
 
     <?php
 }
